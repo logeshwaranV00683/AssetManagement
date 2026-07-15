39ee34b4bdfec5a1622a854060aea32480cd7266
Kesava Sankari M <V00859@VERINITE1.local>
2026-07-15 12:54:10 +0530

    Added Constraint for empID and Password

--- asset-management-api/src/main/java/com/verinite/assetmanagementtool/dto/AdminLoginDto.java
+++ asset-management-api/src/main/java/com/verinite/assetmanagementtool/dto/AdminLoginDto.java
@@ -4,13 +4,18 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 @Getter
 @Setter
 public class AdminLoginDto {
+
     @Pattern(regexp = "^(V\\d{5})$", message = "Must be V followed by 5 digits")
+    @NotNull
     private String empId;
     @NotBlank
+    @Size(min = 8, max = 15, message = "Password must be between 8 and 20 characters")
     private String password;
 }
