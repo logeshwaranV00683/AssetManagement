65ec11824993ca8720c7b1b0d04f7b27c9c65a28
Srivalasalan <manikandan85334@gmail.com>
2026-07-23 15:25:05 +0530

    feat: add unauthorize response body

--- asset-management-api/src/main/java/com/verinite/assetmanagementtool/config/JwtAuthenticationEntryPoint.java
+++ asset-management-api/src/main/java/com/verinite/assetmanagementtool/config/JwtAuthenticationEntryPoint.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.Serial;
 import java.io.Serializable;
+import java.time.OffsetTime;
 
 @Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint, Serializable {
@@ -19,6 +20,19 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint, Se
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response,
                          AuthenticationException authException) throws IOException {
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+//        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+        response.setContentType("application/json");
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+
+        String timestamp= OffsetTime.now().toString();
+
+        response.getWriter().write("""
+                "timestamp": "%s",
+                 "status": 401,
+                 "error": "Unauthorized",
+                 "message": "Invalid or Expired Token"
+                """.formatted(timestamp));
+
+        response.getWriter().flush();
     }
 }
