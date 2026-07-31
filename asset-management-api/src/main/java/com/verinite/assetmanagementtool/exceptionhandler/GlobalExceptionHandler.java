7f4b0699166b3153d19a346f53b2da82daacf37f
Srivalasalan <manikandan85334@gmail.com>
2026-07-31 17:43:15 +0530

    feat: handle error response for invalid date formats

--- asset-management-api/src/main/java/com/verinite/assetmanagementtool/exceptionhandler/GlobalExceptionHandler.java
+++ asset-management-api/src/main/java/com/verinite/assetmanagementtool/exceptionhandler/GlobalExceptionHandler.java
@@ -1,15 +1,17 @@
 package com.verinite.assetmanagementtool.exceptionhandler;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.web.firewall.RequestRejectedException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.ConstraintViolationException;
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -20,9 +22,7 @@ public class GlobalExceptionHandler {
     public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
         Map<String, String> errors = new HashMap<>();
         ex.getBindingResult().getAllErrors().forEach((error) -> {
-            String fieldName = ((FieldError) error).getField();
-            String errorMessage = error.getDefaultMessage();
-            errors.put(fieldName, errorMessage);
+            errors.putIfAbsent(((FieldError) error).getField(),error.getDefaultMessage());
         });
         return errors;
     }
@@ -30,7 +30,7 @@ public class GlobalExceptionHandler {
     @ResponseStatus(HttpStatus.NOT_ACCEPTABLE)
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
 
     @ExceptionHandler(ConstraintViolationException.class)
@@ -42,5 +42,39 @@ public class GlobalExceptionHandler {
             errorMap.put(path, cv.getMessage());
         });
         return errorMap;
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handle(Exception ex) {
+
+        System.out.println(ex.getClass().getName());
+
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(InvalidDateException.class)
+    public Map<String,String> handleInvalidDateException(InvalidDateException ex, HttpServletRequest req){
+        Map<String,String> error=new HashMap<>();
+        error.put("timestamp: ", LocalDateTime.now().toString());
+        error.put("status: ",String.valueOf(HttpStatus.BAD_REQUEST.value()));
+        error.put("error: ",ex.getMessage());
+        error.put("path: ",req.getRequestURI());
+
+        return error;
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public Map<String,String> handleDataIntegrityViolationException(DataIntegrityViolationException ex,HttpServletRequest req){
+
+        Map<String,String> error=new HashMap<>();
+        error.put("timestamp: ",LocalDateTime.now().toString());
+        error.put("status: ",String.valueOf(HttpStatus.BAD_REQUEST.value()));
+        error.put("error: ","Invalid Date Format");
+        error.put("path: ",req.getRequestURI());
+
+        return error;
+
     }
 }
