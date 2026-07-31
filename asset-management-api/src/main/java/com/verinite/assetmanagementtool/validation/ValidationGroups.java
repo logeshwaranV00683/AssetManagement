c04ef226fd11bc6e0e635267875a0e171d49b113
Srivalasalan <manikandan85334@gmail.com>
2026-07-31 17:51:37 +0530

    feat: add groups

---
+++ asset-management-api/src/main/java/com/verinite/assetmanagementtool/validation/ValidationGroups.java
@@ -0,0 +1,11 @@
+package com.verinite.assetmanagementtool.validation;
+
+public interface ValidationGroups {
+
+    interface OnCreate{}
+    interface OnUpdate{}
+    interface OnAssigned{}
+    interface OnForgotViaOtp{}
+    interface OnChangeWithOldPassword{}
+
+}
