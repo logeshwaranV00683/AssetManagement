89c7e485410f67c8c0f1cf6c644e82360bf6058e
Srivalasalan <manikandan85334@gmail.com>
2026-07-31 17:42:01 +0530

    fix: add groups for validation annotations

--- asset-management-api/src/main/java/com/verinite/assetmanagementtool/dto/ResetPasswordDTO.java
+++ asset-management-api/src/main/java/com/verinite/assetmanagementtool/dto/ResetPasswordDTO.java
@@ -1,5 +1,6 @@
 package com.verinite.assetmanagementtool.dto;
 
+import com.verinite.assetmanagementtool.validation.ValidationGroups;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -14,10 +15,12 @@ public class ResetPasswordDTO {
     @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", message = "Invalid email address")
     @NotBlank
     private String mail;
-    @NotBlank
+    @NotBlank(groups = ValidationGroups.OnChangeWithOldPassword.class,message = "Old password is required")
     private String oldPassword;
+    @NotBlank(message = "New password is required")
     @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$", message = "New password is weak")
     private String newPassword;
+    @NotBlank(groups = ValidationGroups.OnForgotViaOtp.class,message = "OTP is required")
     @Pattern(regexp = "^[0-9]{6}$", message = "OTP must be exactly 6 digits")
     private String otp;
 }
