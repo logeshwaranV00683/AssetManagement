89c513642f966e356f8114e3126dbbe4f93a5b08
Srivalasalan <manikandan85334@gmail.com>
2026-07-31 17:38:43 +0530

    fix: add validated instead of valid because of using groups

--- asset-management-api/src/main/java/com/verinite/assetmanagementtool/controller/PasswordResetController.java
+++ asset-management-api/src/main/java/com/verinite/assetmanagementtool/controller/PasswordResetController.java
@@ -2,13 +2,16 @@ package com.verinite.assetmanagementtool.controller;
 
 import com.verinite.assetmanagementtool.dto.ResetPasswordDTO;
 import com.verinite.assetmanagementtool.service.ForgotPasswordInterface;
+import com.verinite.assetmanagementtool.validation.ValidationGroups;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.mail.MessagingException;
 import javax.validation.Valid;
+import javax.validation.groups.Default;
 import java.io.UnsupportedEncodingException;
 
 @RestController
@@ -26,13 +29,13 @@ public class PasswordResetController {
 
     @Operation(summary = "Verify the otp and reset the password, required fields are new Password, otp and mail | Login Not Required")
     @PostMapping("/verify-otp")
-    public ResponseEntity<?> verifyOtpAndResetPassword(@RequestBody @Valid ResetPasswordDTO resetPassword) {
+    public ResponseEntity<?> verifyOtpAndResetPassword(@RequestBody @Validated({Default.class, ValidationGroups.OnForgotViaOtp.class}) ResetPasswordDTO resetPassword) {
         return forgotPasswordInterface.checkOtp(resetPassword);
     }
 
     @Operation(summary = "Change the password using only old password without otp, require fields are new password, old password and mail | Login is Required")
     @PostMapping("/change-password-using-oldPassword")
-    public ResponseEntity<?> changePasswordWithCurrent(@RequestBody @Valid ResetPasswordDTO changePassword) {
+    public ResponseEntity<?> changePasswordWithCurrent(@RequestBody @Validated({Default.class,ValidationGroups.OnChangeWithOldPassword.class}) ResetPasswordDTO changePassword) {
         return forgotPasswordInterface.changePassword(changePassword);
     }
 }
